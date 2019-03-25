6fdf1d421278ba0e1430cdc9c30d763ff6ddafb9
howard Lee <duanhaowei@hotmail.com>
2019-03-25 15:10:41 +0800

    123

---
+++ src/main/java/com/leedarson/HelloController.java
@@ -0,0 +1,23 @@
+package com.leedarson;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+
+@RestController
+public class HelloController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/hello")
+	public JSONObject greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		JSONObject jo = new JSONObject();
+		jo.put(String.valueOf(counter.incrementAndGet()), String.format(template, name));
+		return jo;
+	}
+}
---
+++ src/main/java/com/leedarson/TestjksApplication.java
@@ -0,0 +1,13 @@
+package com.leedarson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestjksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestjksApplication.class, args);
+	}
+
+}
